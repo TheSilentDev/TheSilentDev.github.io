d179b2f1ea9b874e7a095f87734bb3e67ebb1999
The Silent Dev <tsd@thesilentdev.me>
2018-12-12 11:21:25 +0200

    Decoy Porfolio page

---
+++ precache-manifest.cb9a8ce4517564a09ae1bc5f40aa53ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "75a46e9590eb78fdab18",
+    "url": "/static/js/main.75a46e95.chunk.js"
+  },
+  {
+    "revision": "f43d292f7adc64a4fb01",
+    "url": "/static/js/1.f43d292f.chunk.js"
+  },
+  {
+    "revision": "75a46e9590eb78fdab18",
+    "url": "/static/css/main.f7f31d8b.chunk.css"
+  },
+  {
+    "revision": "f43d292f7adc64a4fb01",
+    "url": "/static/css/1.64924949.chunk.css"
+  },
+  {
+    "revision": "63a0ac78dc91d9bdb0a6242cc9b318d4",
+    "url": "/index.html"
+  }
+];
